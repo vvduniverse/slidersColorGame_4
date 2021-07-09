1d4e5fac86fb10ef8025ddb5ba9883112bfcd2c7
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-09 20:58:56 +0300

    Version 1 of implementation

--- slidersColorGame/ViewController.swift
+++ slidersColorGame/ViewController.swift
@@ -18,19 +18,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var redColorSlider: UISlider!
     @IBOutlet weak var greenColorSlider: UISlider!
     @IBOutlet weak var blueColorSlider: UISlider!
+    
+//    private var redColor = CGFloat(redColorSlider.value)
         
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
+        redColorSlider.value = 0.5
+        redColorSlider.minimumValue = 0
+        redColorSlider.maximumValue = 1
+        redColorSlider.minimumTrackTintColor = .red
+        
+        greenColorSlider.value = 0.5
+        greenColorSlider.minimumValue = 0
+        greenColorSlider.maximumValue = 1
+        greenColorSlider.minimumTrackTintColor = .green
+        
+        blueColorSlider.value = 0.5
+        blueColorSlider.minimumValue = 0
+        blueColorSlider.maximumValue = 1
+        blueColorSlider.minimumTrackTintColor = .blue
+
     }
+    
+//    let color = UIColor(red: 0.8, green: 0.1, blue: 0.5, alpha: 1)
 
     @IBAction func redColorSet() {
+        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
+                                                 green: CGFloat(greenColorSlider.value),
+                                                 blue: CGFloat(blueColorSlider.value),
+                                                 alpha: 1)
     }
     
     @IBAction func greenColorSet() {
+        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
+                                                 green: CGFloat(greenColorSlider.value),
+                                                 blue: CGFloat(blueColorSlider.value),
+                                                 alpha: 1)
     }
     
     @IBAction func blueColorSet() {
+        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
+                                                 green: CGFloat(greenColorSlider.value),
+                                                 blue: CGFloat(blueColorSlider.value),
+                                                 alpha: 1)
     }
 }
 
