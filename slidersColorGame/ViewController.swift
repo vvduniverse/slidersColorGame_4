d21b969fff677a35fea10001a1859ecbed1ce16c
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-10 08:02:57 +0300

    Version 2 of the final implementation

--- slidersColorGame/ViewController.swift
+++ slidersColorGame/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var viewColorRange: UIView!
-    
 
     @IBOutlet weak var redColorValue: UILabel!
     @IBOutlet weak var greenColorValue: UILabel!
@@ -19,56 +18,71 @@ class ViewController: UIViewController {
     @IBOutlet weak var redColorSlider: UISlider!
     @IBOutlet weak var greenColorSlider: UISlider!
     @IBOutlet weak var blueColorSlider: UISlider!
-    
-//    private var redColor = CGFloat(redColorSlider.value)
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
         redColorSlider.value = 0.5
-        redColorSlider.minimumValue = 0
-        redColorSlider.maximumValue = 1
-        redColorSlider.minimumTrackTintColor = .red
-        
         greenColorSlider.value = 0.5
-        greenColorSlider.minimumValue = 0
-        greenColorSlider.maximumValue = 1
-        greenColorSlider.minimumTrackTintColor = .green
-        
         blueColorSlider.value = 0.5
+        
+        redColorSlider.minimumValue = 0
+        greenColorSlider.minimumValue = 0
         blueColorSlider.minimumValue = 0
+        
+        redColorSlider.maximumValue = 1
+        greenColorSlider.maximumValue = 1
         blueColorSlider.maximumValue = 1
+        
+        redColorSlider.minimumTrackTintColor = .red
+        greenColorSlider.minimumTrackTintColor = .green
         blueColorSlider.minimumTrackTintColor = .blue
         
         redColorValue.text = String(redColorSlider.value)
         greenColorValue.text = String(greenColorSlider.value)
         blueColorValue.text = String(blueColorSlider.value)
         
+        viewColorRange.layer.cornerRadius = 15
 
     }
-    
-//    let color = UIColor(red: 0.8, green: 0.1, blue: 0.5, alpha: 1)
 
     @IBAction func redColorSet() {
-        redColorValue.text = String(redColorSlider.value)
-        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
-                                                 green: CGFloat(greenColorSlider.value),
-                                                 blue: CGFloat(blueColorSlider.value),
-                                                 alpha: 1)
+        
+        let redValue = String(round(redColorSlider.value * 100) / 100)
+        let redColor = UIColor(red: CGFloat(redColorSlider.value),
+                               green: CGFloat(greenColorSlider.value),
+                               blue: CGFloat(blueColorSlider.value),
+                               alpha: 1)
+        
+        redColorValue.text = redValue
+        viewColorRange.backgroundColor = redColor
     }
+    //        я думал о том, чтобы выделить все строки логическими отступами, но тогда слишком разреженно выглядит код. поэтому оставил так :)
+    //        делать ли эти свойства приватными? я не увидел возможности обратиться к этой функции дальше... или я что-то не понял :)
     
     @IBAction func greenColorSet() {
-        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
-                                                 green: CGFloat(greenColorSlider.value),
-                                                 blue: CGFloat(blueColorSlider.value),
-                                                 alpha: 1)
+        
+        let greenValue = String(round(greenColorSlider.value * 100) / 100)
+        let greenColor = UIColor(red: CGFloat(redColorSlider.value),
+                               green: CGFloat(greenColorSlider.value),
+                               blue: CGFloat(blueColorSlider.value),
+                               alpha: 1)
+        
+        greenColorValue.text = greenValue
+        viewColorRange.backgroundColor = greenColor
     }
     
     @IBAction func blueColorSet() {
-        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
-                                                 green: CGFloat(greenColorSlider.value),
-                                                 blue: CGFloat(blueColorSlider.value),
-                                                 alpha: 1)
+        
+        let blueValue = String(round(blueColorSlider.value * 100) / 100)
+        let blueColor = UIColor(red: CGFloat(redColorSlider.value),
+                                green: CGFloat(greenColorSlider.value),
+                                blue: CGFloat(blueColorSlider.value),
+                                alpha: 1)
+        
+        blueColorValue.text = blueValue
+        viewColorRange.backgroundColor = blueColor
     }
+    
 }
 
