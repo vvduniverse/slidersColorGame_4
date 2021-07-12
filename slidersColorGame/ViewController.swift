38b9953da45e0c7c7a683752b3a9f615b88fec75
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-12 13:22:18 +0300

    Version 2.4.1,  corrected

--- slidersColorGame/ViewController.swift
+++ slidersColorGame/ViewController.swift
@@ -17,18 +17,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         viewColorRange.layer.cornerRadius = 15
-        
-        redColorSlider.value = 0.5
-        greenColorSlider.value = 0.5
-        blueColorSlider.value = 0.5
-        
-        redColorSlider.minimumValue = 0
-        greenColorSlider.minimumValue = 0
-        blueColorSlider.minimumValue = 0
-        
-        redColorSlider.maximumValue = 1
-        greenColorSlider.maximumValue = 1
-        blueColorSlider.maximumValue = 1
+        viewColorRange.backgroundColor = .darkGray
+//        viewColorRange.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
+//                                                 green: CGFloat(greenColorSlider.value),
+//                                                 blue: CGFloat(blueColorSlider.value),
+//                                                 alpha: 1)
         
         redColorSlider.minimumTrackTintColor = .red
         greenColorSlider.minimumTrackTintColor = .green
@@ -41,31 +34,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func redColorSet() {
-        
         let redColorValue = String(round(redColorSlider.value * 100) / 100)
         
         redColorValueLabel.text = redColorValue
-        
         backgroundColors()
     }
-    //        я думал о том, чтобы выделить все строки логическими отступами, но тогда слишком разреженно выглядит код. поэтому оставил так :)
-    //        делать ли эти свойства приватными? я не увидел возможности обратиться к этой функции дальше... или я что-то не понял :)
     
     @IBAction func greenColorSet() {
-        
         let greenColorValue = String(round(greenColorSlider.value * 100) / 100)
         
         greenColorValueLabel.text = greenColorValue
-        
         backgroundColors()
     }
     
     @IBAction func blueColorSet() {
-        
         let blueColorValue = String(round(blueColorSlider.value * 100) / 100)
       
         blueColorValueLabel.text = blueColorValue
-        
         backgroundColors()
     }
     
